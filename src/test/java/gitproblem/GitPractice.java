a42aaa08824c4f36bb13c78ef3ce0544283b8f22
Ratan2007 <skratan2007@gmail.com>
2023-02-13 08:16:38 -0500

    crat1 changes

--- src/test/java/gitproblem/GitPractice.java
+++ src/test/java/gitproblem/GitPractice.java
@@ -4,9 +4,7 @@ public class GitPractice {
 public void hafizgit1() {
 	System.out.println("hafizgit1");
 }
-public void hafizgit2() {
-	System.out.println("hafizgit2");
-}
+
 public void rat1() {
 	System.out.println("rat1");
 }
